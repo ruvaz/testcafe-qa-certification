e4bf3d3228f6a69d96cfd49ba162b8cc0811a0f4
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2021-03-15 18:38:31 -0600

    starting point

---
+++ pages/LoginPage.js
@@ -0,0 +1,10 @@
+import {Selector} from 'testcafe'
+
+export default class LoginPage {
+
+    constructor() {
+        this.userInput = Selector("#user-name")
+        this.passwordInput = Selector('#password')
+        this.loginButton = Selector('#login-button')
+    }
+}
